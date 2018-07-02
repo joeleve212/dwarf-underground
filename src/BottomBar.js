07d0e7d04f1df47fb3624178532f5dcab63aa0ec
unknown <Joe Leveille>
2018-07-02 14:48:15 -0400

    Add hella components

---
+++ src/BottomBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const BottomBar = () =>{
+    return(
+        <footer className="expanded row">
+      <h6>&copy; 2017 Erebor Industries</h6>
+      <h6 className="float-right">All rights reserved</h6>
+    </footer>
+    );
+}
+export default BottomBar
---
+++ src/ToolbarButtons.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const ToolbarButtons = () => {
+    return(
+    <div className="clearfix">
+            <div className="float-left logo">
+              <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
+            </div>
+            <div className="float-right nav-links">
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Log in</a>
+            </div>
+          </div>
+    );}
+    export default ToolbarButtons
